3d85ef38119b537d6f663292d7b6da3be74e0628
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-16 21:42:45 +0800

    Update vector.cpp

--- c++STL/vector.cpp
+++ c++STL/vector.cpp
@@ -28,7 +28,7 @@ int main()
 	//  (1)输入迭代器(Input Iterator):只能向前单步迭代元素，不允许修改由该迭代器所引用的元素
 	//  (2)输出迭代器(Output Iterator):只能向前单步迭代元素，对由该迭代器所引用的元素只有写权限
 	//  (3)向前迭代器(Forward Iterator):该迭代器可以在一个区间中进行读写操作，它拥有输入迭代器的所有特性和输出迭代器的部分特性，以及向前单步迭代元素的能力(只能++或--)
-	//  (4)双向迭代器(Bidirectional Iterator):在向前迭代器的基础上增加了向后单步迭代元素的能力
+	//  (4)双向迭代器(Bidirectional Iterator):在向前迭代器的基础上增加了向后单步迭代元素的能力,All bidirectional iterators are also valid forward and input iterators.
 	//  (5)随机访问迭代器(Random Access Iterator):不仅综合以后4种迭代器的所有功能，还可以像指针那样进行算术计算
 	//
 	//  定義方式:
