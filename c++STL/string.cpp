efae2981a73e9c7d67a7bf7c5f70b661c6d275aa
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-21 18:36:36 +0800

    Update string.cpp

--- c++STL/string.cpp
+++ c++STL/string.cpp
@@ -20,8 +20,9 @@ int main()
 	str4 = str1; //easy to copy
 	str4 > str1;//compare two strings character by character,if same->next char,else->by ASCII
 
-	//length of the string
-	str2.length();
+	//length of the string (Both string::size and string::length are synonyms and return the exact same value.)
+	str2.length();  //The method string::length returns the length of the string, in terms of bytes.
+	str2.size();    //The method string::size returns the length of the string, in terms of bytes.
 
 	//empty or not
 	str2.empty();
@@ -29,8 +30,10 @@ int main()
 	//some member function
 	str1.append(str2); //(拼接)append str2 after str1 (Extends the string by appending additional characters at the end of its current value)
 	str1.append(str2, 0, 2); //append str2 range[0,2) to str1  
-	/*ex:string reptile = "catapillar"; string mammal = "whale"; 
-	     reptile.append(mammal,0,5); -> catapillarwhale*/
+	/*
+	//ex:string reptile = "catapillar"; string mammal = "whale"; 
+	//   reptile.append(mammal,0,5); -> catapillarwhale
+	*/
 	str2.erase(0,3); //delete char range: [0,3) 
 	str1.swap(str3); //str1<->str3
 	str1.clear(); //delete all char in the string
@@ -41,7 +44,15 @@ int main()
 	str1.compare(str2); //same->0,difference->1
 
 	//type in
-	//cin will stop when there is enter or space
+	//cin will stop when there is enter or space, so we need getline
+	/*
+	//istream& getline(istream& is, string& str, char delim);
+	//is:它是istream類的對象，它告訴函數有關從何處讀取輸入的流。
+	//str:這是一個字符串對象，從流中讀取輸入後，將輸入存儲在此對象中。
+	//delim:它是定界字符，它告訴函數在達到該字符後停止讀取進一步的輸入。(default:'\n')
+	//If the delimiter is found, it is extracted and discarded (i.e. it is not stored and the next input operation will begin after it).
+	//当 cin 读取数据时，它会传递并忽略任何前导白色空格字符（空格、制表符或换行符）。一旦它接触到第一个非空格字符即开始阅读，当它读取到下一个空白字符时，它将停止读取
+	*/
 	getline(cin, str4); //can have space ! can continue typing
 	(cin>>store).get(); //if continue typing,we can use this method to ignore enter
 
