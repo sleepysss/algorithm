cdec9ea6e233250eb12abc1823e474848deb1138
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-20 12:29:14 +0800

    Update shortestpath_Dijkstra_Algorithm.cpp

--- greedy/shortestpath_Dijkstra_Algorithm.cpp
+++ greedy/shortestpath_Dijkstra_Algorithm.cpp
@@ -11,6 +11,20 @@ using namespace std;
 int G[6][6] = { {MAX,1,12,MAX,MAX,MAX},{MAX,MAX,9,3,MAX,MAX},{MAX,MAX,MAX,MAX,5,MAX},{MAX,MAX,4,MAX,13,15},{MAX,MAX,MAX,MAX,MAX,4},{MAX,MAX,MAX,MAX,MAX,MAX} };
 int Distance[6] = { 0 ,MAX ,MAX ,MAX ,MAX ,MAX }; //distance from start to other vertex //we dont know the distance at the begining
 bool choosen[6] = { 0 }; //vertex is choosen or not
+int pre[6]={ -1 ,-1 ,-1 ,-1 ,-1 ,-1 }; //pre-node
+
+
+void printPath(int start_vertex)
+{
+    if(pre[start_vertex]==-1)
+    {
+        cout<<start_vertex<<" ";
+        return;
+    }
+    printPath(pre[start_vertex]);
+    cout<<start_vertex<<" ";
+}
+
 
 void ShortestPath(int start_vertex)
 {
@@ -40,7 +54,10 @@ void ShortestPath(int start_vertex)
 		{
 			if (choosen[i]) continue;  //we dont need to update the Distance of the vertex we have choosen
 			if(Distance[i] > (Distance[smallest_vertex] + G[smallest_vertex][i]))  //have shorter Distance or not
-				Distance[i] = (Distance[smallest_vertex] + G[smallest_vertex][i]);
+			{
+			    pre[i]=smallest_vertex;
+			    Distance[i] = (Distance[smallest_vertex] + G[smallest_vertex][i]);
+			}
 		}
 	}
 }
@@ -50,7 +67,32 @@ int main()
 	ShortestPath(0);
 	cout << "shortest path from 0 to " << endl<<endl;
 	for (int i = 0; i < 6; ++i)
-		cout << i << " " <<"Distance: "<< Distance[i] << endl;
-	system("pause");
+	{
+	    cout << i << " " <<"Distance: "<< Distance[i] << endl;
+	    cout << "Path: ";
+	    printPath(i);
+	    cout<<endl<<endl;
+	}
+	
+	//shortest path from 0 to 
+
+	//0 Distance: 0
+	//Path: 0 
+
+	//1 Distance: 1
+	//Path: 0 1 
+
+	//2 Distance: 8
+	//Path: 0 1 3 2 
+
+	//3 Distance: 4
+	//Path: 0 1 3 
+
+	//4 Distance: 13
+	//Path: 0 1 3 2 4 
+
+	//5 Distance: 17
+	//Path: 0 1 3 2 4 5 
+	
 	return 0;
 }
